ccc6485dd321f66c4166b36014c35dda6a1274cd
HuwRiley <huwriley1@gmail.com>
2025-11-12 10:58:22 +0000

    bugs fixed

--- scripts/who2.R
+++ scripts/who2.R
@@ -1,8 +1,9 @@
 library(tidyr)
+library(dplyr)
 library(ggplot2)
 data("who", package = "tidyr")
+country <- filter(who, iso2 == "NA")
 
-country <- filter(who, iso2 == 'NA')
 
 ggplot(country, aes(x = year, y = new_sp_m014)) +
   geom_line() +
